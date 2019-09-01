78f716ee152736a9b73d86d47f47b1aa4b951077
soycurd <soycurd1@gmail.com>
2019-09-01 18:08:10 +0900

    fix doc

--- jetson/setup.sh
+++ jetson/setup.sh
@@ -4,11 +4,16 @@
 # AC電源 & wifiドングル & カメラ無し -> ネットワーク接続時に落ちる
 # USB電源 > 2A & wifiドングル & カメラ無し -> 起動
 
+# install utility
 mkdir workspace
 git clone https://github.com/staminajiro/shoten_7
 
 sudo apt update
 sodu apt install -y curl nano
+
+# disable capslock
+gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+sudo sed -i -e "s/XKBOPTIONS=\"\"/XKBOPTIONS=\"ctrl:nocaps\"/g" /etc/default/keyboard
 
 # make tmp
 mkdir tmp
@@ -29,3 +34,16 @@ cd jetson-nano-tools
 cd ~/workspace/tmp
 wget https://nvidia.box.com/shared/static/veo87trfaawj5pfwuqvhl6mzc5b55fbj.whl -O torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
 pip3 install numpy torch-1.1.0a0+b457266-cp36-cp36m-linux_aarch64.whl
+
+# install other lib
+pip3 install jupyter
+sudo apt -y install libjpeg-dev libfreetype6-dev pkg-config libpng-dev
+pip3 install matplotlib pillow==5.4.1
+
+# install torch vision
+# https://roshiago.blog.so-net.ne.jp/2019-05-04
+cd ~/workspace/tmp
+sudo apt install -y libjpeg-dev zlib1g-dev
+git clone -b v0.3.0 https://github.com/pytorch/vision torchvision
+cd torchvision
+sudo python3 setup.py install
