a3a7beacf89424029a978a7eb152610350fa938d
soycurd <soycurd1@gmail.com>
2019-09-01 20:45:48 +0900

    fix setup

--- jetson/setup.sh
+++ jetson/setup.sh
@@ -15,6 +15,56 @@ sodu apt install -y curl nano
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
 sudo sed -i -e "s/XKBOPTIONS=\"\"/XKBOPTIONS=\"ctrl:nocaps\"/g" /etc/default/keyboard
 
+# remote desktop
+# https://blog.hackster.io/getting-started-with-the-nvidia-jetson-nano-developer-kit-43aa7c298797
+# https://qiita.com/iwatake2222/items/a3bd8d0527dec431ef0f
+cd {your git repo}/jetson
+
+# sudo apt install -y xfce4
+# echo xfce4-session >~/.xsession
+# sudo cp startwm.sh.sample /etc/xrdp/startwm.sh
+# sudo service xrdp restart
+# -> can not launch app in xfce!!!!
+
+#new_cursorsの無効化
+#sudo sed -e 's/^new_cursors=true/new_cursors=false/g' \
+# -i /etc/xrdp/xrdp.ini
+#  
+
+#xrdpサービスの再起動
+# sudo systemctl restart xrdp
+ 
+#xsessionファイルの作成
+# D=/usr/share/ubuntu:/usr/local/share:/usr/share:/var/lib/snapd/desktop
+# cat <<EOF > ~/.xsessionrc
+#  export GNOME_SHELL_SESSION_MODE=ubuntu
+#  export XDG_CURRENT_DESKTOP=ubuntu:GNOME
+#  export XDG_DATA_DIRS=${D}
+#  export XDG_CONFIG_DIRS=/etc/xdg/xdg-ubuntu:/etc/xdg
+# EOF
+ 
+#Authentication Requiredダイアログの回避
+# cat <<EOF | \
+#    sudo tee /etc/polkit-1/localauthority/50-local.d/xrdp-color-manager.pkla
+#  [Netowrkmanager]
+#  Identity=unix-user:*
+#  Action=org.freedesktop.color-manager.create-device
+#  ResultAny=no
+#  ResultInactive=no
+#  ResultActive=yes
+# EOF
+ 
+# sudo systemctl restart polkit
+# -> can not config ~/.xsessionrc ....
+
+# add user
+# sudo adduser jetson2
+# sudo gpasswd -a jetson2 sudo
+
+# install terminology
+# sudo add-apt-repository ppa:enlightenment-git/ppa
+# sudo apt install -y terminology
+
 # make tmp
 mkdir tmp
 
