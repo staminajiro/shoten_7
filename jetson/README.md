998950bb5a54f3ced759dabab08b53542cb5834c
soy <soy@soynoMacBook-Air.local>
2019-09-01 11:59:44 +0900

    add jetson dir

---
+++ jetson/README.md
@@ -0,0 +1,8 @@
+jetson セットアップ用
+====
+
+## パーツ
++ wifi: WLI-UC-GNM2S(2019/0901 ドライバビルド無しで接続確認)
+
+## セットアップ
++ からあげさん記事参照(https://qiita.com/karaage0703/items/b09f3c65eb3913d087e4)
---
+++ jetson/setup.sh
@@ -0,0 +1,9 @@
+
+# マウス、キーボード、hdmi、電源を接続して起動
+# この時点でethernetを接続し、起動しない事象有り -> 電源不足説かも
+# AC電源 & wifiドングル -> ネットワーク接続時に落ちる
+# AC電源 & wifiドングル & カメラ無し -> ネットワーク接続時に落ちる
+# USB電源 > 2A & wifiドングル & カメラ無し -> 起動
+
+mkdir workspace
+git clone https://github.com/staminajiro/shoten_7
