72064dda7c6c9b48b8e6b8244bc88b7f44c081de
soy <soy@soynoMacBook-Air.local>
2019-09-10 23:05:28 +0900

    add chap03

--- doc/pdf.sh
+++ doc/pdf.sh
@@ -3,6 +3,5 @@ rm -rf book-pdf
 rm -rf review
 mkdir review
 
-docker run --rm -v `pwd`/.:/work nuitsjp/md2review /bin/sh -c "cd /work && md2review src/chap00.md > review/chap00.re"
-docker run --rm -v `pwd`/.:/work nuitsjp/md2review /bin/sh -c "cd /work && md2review src/chap01.md > review/chap01.re"
+ls src | awk  -F. '{print "docker run --rm -v \`pwd\`/.:/work nuitsjp/md2review /bin/sh -c \"cd /work && md2review src/" $1 "." $2 " > review/" $1 ".re\"" }' | bash
 docker run --rm -v `pwd`/.:/work vvakame/review /bin/sh -c "cd /work && review-pdfmaker config.yml"
