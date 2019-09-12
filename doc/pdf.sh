25c551ba4710de653b80115bba2c18665f87f759
soy <soycurd1@gmail.com>
2019-09-12 11:53:21 +0900

    remove escape

--- doc/pdf.sh
+++ doc/pdf.sh
@@ -3,5 +3,5 @@ rm -rf book-pdf
 rm -rf review
 mkdir review
 
-ls src | awk  -F. '{print "docker run --rm -v \`pwd\`/.:/work nuitsjp/md2review /bin/sh -c \"cd /work && md2review src/" $1 "." $2 " > review/" $1 ".re\"" }' | bash
+ls src | awk  -F. '{print "docker run --rm -v `pwd`/.:/work nuitsjp/md2review /bin/sh -c \"cd /work && md2review src/" $1 "." $2 " > review/" $1 ".re\"" }' | bash
 docker run --rm -v `pwd`/.:/work vvakame/review /bin/sh -c "cd /work && review-pdfmaker config.yml"
